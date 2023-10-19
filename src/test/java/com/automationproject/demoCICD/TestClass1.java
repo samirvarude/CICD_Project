a0907842229f418566fdbf3aea65cb3d4fb218a0
samirvarude <samir.varude@gmail.com>
2023-10-19 14:33:24 +0100

    SeleniumGrid

--- src/test/java/com/automationproject/demoCICD/TestClass1.java
+++ src/test/java/com/automationproject/demoCICD/TestClass1.java
@@ -1,10 +1,15 @@
 package com.automationproject.demoCICD;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,12 +18,18 @@ public class TestClass1 {
 	public static WebDriver driver;
 	
 	@BeforeMethod
-	public void launchDriver()
+	public void launchDriver() throws MalformedURLException
 	{
+		DesiredCapabilities dr=new DesiredCapabilities();
+		dr.setBrowserName("chrome");
+		dr.setPlatform(Platform.WIN10);
+		
+		
 		ChromeOptions ops = new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
 		System.setProperty("webdriver.chrome.driver","C:\\CICD\\ChromeDriver\\chromedriver.exe");
-		driver=new ChromeDriver(ops);
+		driver=new RemoteWebDriver(new URL("http://192.168.0.44:4444/wd/hub"),dr);
+		//driver=new ChromeDriver(ops);
 		driver.get("https://login.salesforce.com/?locale=eu");
 		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		//driver.manage().window().fullscreen();
